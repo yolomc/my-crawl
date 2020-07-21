923c5099cf1f2d48c36cc80c01efc5fca1c7694f
yolomc <yolo.mc@icloud.com>
2020-07-22 00:29:09 +0800

    my-crawl

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"github.com/yolomc/my-crawl/engine"
-	"github.com/yolomc/my-crawl/parse"
+	"fmt"
 )
 
 func main() {
-
-	// content, err := fetcher.Fetch("https://www.bilibili.com/ranking/all/129/0/3")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	//fetcher.ParseContent(content)
-	//fmt.Printf("%s", content)
-
-	engine.Run(engine.Request{
-		Url:       "https://book.douban.com/tag/",
-		ParseFunc: parse.ParseContent,
-	})
-
+	fmt.Println("my-crawl")
 }
